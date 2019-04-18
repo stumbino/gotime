3127b862b3a43f01471ac88cda238245eb5020e6
bcompto <4comptob@gmail.com>
2019-04-18 14:18:47 -0400

    see list of events

--- src/client/components/DashboardPage.js
+++ src/client/components/DashboardPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import EventSummary from './EventsSummary';
 import {connect} from 'react-redux';
+import EventList from './EventList';
 
 const DashboardPage = (props) => {
     console.log(props);
     return(
         <div>
             <EventSummary />
+            <EventList />
         </div>
     );
 };
